cf0d004a90a017e8d149468d6b0c743397642898
Zuhaib Javed <150413091+Zuhaibj@users.noreply.github.com>
2023-12-26 16:30:20 +0500

    Add files via upload

---
+++ cal.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer"
+
+const answer:{
+    num1:number;
+    num2:number;
+    operator: string
+} = await inquirer.prompt([
+{
+    type:"number",
+    name: "num1",
+    message: 'Kindly input your 1st number'
+},
+{
+    type:"number",
+    name: "num2",
+    message: 'Kindly input your 2nd number'
+},
+{
+    type:"list",
+    name: "operator",
+    message: 'What operations you want to perform?',
+    choices:['Addition','Subtraction','Multiplication','Division']
+}
+])
+
+if (answer.operator === 'Addition'){
+    console.log(answer.num1 + answer.num2);
+} else if (answer.operator === 'Subtraction'){
+    console.log(answer.num1 - answer.num2);
+} else if (answer.operator === 'Multiplication'){
+    console.log(answer.num1 * answer.num2);
+} else if (answer.operator === 'Division'){
+    console.log(answer.num1 / answer.num2);
+}
